c9b934316b9b04a84b11565f3cd19763cbdd7a78
Melkemunto <melissakemunto18@gmail.com>
2022-09-12 05:02:32 -0700

    corr 2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,7 @@
 /*
  * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art,
+ * Description: this program shows the use of printf
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
